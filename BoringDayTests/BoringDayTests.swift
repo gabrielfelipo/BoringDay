66e65c132cb3f75ec10af19e0fd47daa2175ed10
Felipo <gabriel.felipo2010@gmail.com>
2023-02-14 16:57:47 -0300

    Making tests

---
+++ BoringDayTests/BoringDayTests.swift
@@ -0,0 +1,17 @@
+//
+//  BoringDayTests.swift
+//  BoringDayTests
+//
+//  Created by gabrielfelipo on 14/02/23.
+//
+
+import XCTest
+
+final class BoringDayTests: XCTestCase {
+
+    func testExample() {
+        XCTAssertEqual(5+4, 9)
+        XCTAssertEqual(9-4, 5)
+    }
+
+}
